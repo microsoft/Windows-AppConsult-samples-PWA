cdd47cb316ed8c7338bf126c8430ca2e38665780
Matteo Pagani <info@qmatteoq.com>
2018-06-06 23:16:03 +0200

    Added new sample to support push notifications in a PWA app runnong in Windows 10

---
+++ ToastActivationWithPushNotifications/PushTestserver/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace PushTestserver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNewsId = new System.Windows.Forms.TextBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(512, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send Push";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnSendPushNotification);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(102, 11);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(383, 26);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Text";
+            // 
+            // txtText
+            // 
+            this.txtText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtText.Location = new System.Drawing.Point(102, 49);
+            this.txtText.Margin = new System.Windows.Forms.Padding(2);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(383, 26);
+            this.txtText.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "News Id";
+            // 
+            // txtNewsId
+            // 
+            this.txtNewsId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNewsId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNewsId.Location = new System.Drawing.Point(102, 84);
+            this.txtNewsId.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNewsId.Name = "txtNewsId";
+            this.txtNewsId.Size = new System.Drawing.Size(383, 26);
+            this.txtNewsId.TabIndex = 7;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(102, 118);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(383, 26);
+            this.txtUrl.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 176);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNewsId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Push Test Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNewsId;
+        private System.Windows.Forms.TextBox txtUrl;
+    }
+}
+
---
+++ ToastActivationWithPushNotifications/PushTestserver/Form1.cs
@@ -0,0 +1,104 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Windows.Forms;
+using PushSharp.Windows;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace PushTestserver
+{
+    public partial class Form1 : Form
+    {
+        string secret = "CyazfQ4UXylFNIDBj1WqHEbXSpsForss"; // need to replace this with your own secret
+        string sid = "ms-app://s-1-15-2-1739691208-2413059114-2912612588-87913382-4138524900-106472690-871635211";    // need to replace this with your own sid
+        string package = "29949MatteoPagani.ExpenseIt-Test"; // need to replace this with your package family name;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSendPushNotification(object sender, EventArgs e)
+        {
+            SendNotification(txtUrl.Text, txtTitle.Text, txtText.Text, txtNewsId.Text);
+        }
+
+        public void SendNotification(string uri, string title, string text, string newsId)
+        {
+            var config = new WnsConfiguration(package, sid, secret);
+            var wnsBroker = new WnsServiceBroker(config);
+
+            wnsBroker.OnNotificationFailed += (notification, aggregateEx) => {
+
+                aggregateEx.Handle(ex => {
+
+                    // See what kind of exception it was to further diagnose
+                    if (ex is WnsNotificationException)
+                    {
+                        var notificationException = (WnsNotificationException)ex;
+                        Debug.WriteLine($"WNS Notification Failed: {notificationException.Message}");
+                    }
+                    else
+                    {
+                        Debug.WriteLine("WNS Notification Failed for some (Unknown Reason)");
+                    }
+
+                    // Mark it as handled
+                    return true;
+                });
+            };
+
+            wnsBroker.OnNotificationSucceeded += (notification) => {
+                Debug.WriteLine("WNS Notification Sent!");
+            };
+
+            // Start the broker
+            wnsBroker.Start();
+            string payload = $"<toast launch=\"{newsId}\"><visual><binding template=\"ToastGeneric\"><text>{title}</text><text>{text}</text></binding></visual></toast>";
+
+            string tile = $@"<tile>
+                               <visual>
+                                    <binding template='TileMedium'>
+                                        <text hint-style='subtitle'>{title}</text>
+                                        <text hint-style='captionSubtle'>{text}</text>
+                                    </binding>
+                                    <binding template='TileWide'>
+                                        <text hint-style='subtitle'>{title}</text>
+                                        <text hint-style='captionSubtle'>{text}</text>
+                                    </binding>
+                                    <binding template='TileLarge'>
+                                        <text hint-style='subtitle'>{title}</text>
+                                        <text hint-style='captionSubtle'>{text}</text>
+                                    </binding>
+                             </visual>
+                       </tile>";
+
+            WnsToastNotification toastNotification = new WnsToastNotification
+            {
+                ChannelUri = uri,
+                Payload = XElement.Parse(payload)
+            };
+
+            WnsTileNotification tileNotification = new WnsTileNotification
+            {
+                ChannelUri = uri,
+                Payload = XElement.Parse(tile)
+            };
+
+            wnsBroker.QueueNotification(toastNotification);
+            wnsBroker.QueueNotification(tileNotification);
+
+            wnsBroker.Stop();
+        }
+    }
+}
